7564253a6140f2291ba02a398f5997acbdba06af
Ambrose Blay <ab2838@cornell.edu>
2026-03-19 23:35:41 -0400

    feat: replace static print with dynamic command output using popen
    Add stdlib.h include and use popen("ls", "r") to execute command, read
    output via fgets into buffer, print each line prefixed with "Command
    Output: ", handle popen failure with perror and early return, close with
    pclose. Add empty tfolder/tfile.

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-    printf("main\n");
+    FILE *fp;
+
+    char output_buffer[128];
+
+    const char *command = "ls";
+
+    fp = popen(command, "r");
+    if (fp == NULL) {
+        perror("popen failed");
+        return EXIT_FAILURE;
+    }
+
+    while (fgets(output_buffer, sizeof(output_buffer), fp) != NULL) {
+        printf("Command Output: %s", output_buffer);
+    }
+
+    pclose(fp);
+    
     return 0;
 }
